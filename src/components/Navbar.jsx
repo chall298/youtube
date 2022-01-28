c900141c4920e83d7320cba0da7ecb66b7c8fcc8
chall298 <chall361@gmail.com>
2022-01-27 23:59:59 -0500

    edited input

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between
 `
 const Left = styled.div`
@@ -23,9 +24,15 @@ const Language = styled.span`
     cursor: pointer;
 `
 const SearchContainer = styled.div`
-    border: 1px solid lightgray;
+    border: 0.5px solid lightgray;
     display: flex;
     align-items: center;
+    margin-left: 25px;
+    padding: 5px
+`
+
+const Input = styled.input`
+    border: none;
 `
 
 const Center = styled.div`
@@ -42,7 +49,7 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        input 
+                        <Input/> 
                         <Search></Search>
                     </SearchContainer>
                 </Left>
