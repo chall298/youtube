57b15c9548c2caef865ff9d734f79f28c2009534
chall298 <chall361@gmail.com>
2022-02-23 01:57:42 -0500

    added shopping cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import {Search} from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import {Search, ShoppingCartOutlined} from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
@@ -45,7 +46,14 @@ const Logo = styled.h1`
 `
 
 const Right = styled.div`
-    flex: 1
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end
+`
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
 `
 
 const Navbar = () => {
@@ -60,7 +68,15 @@ const Navbar = () => {
                     </SearchContainer>
                 </Left>
                 <Center><Logo>LAMA.</Logo></Center>
-                <Right>right</Right>
+                <Right>
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>Sign In</MenuItem>
+                    <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined />
+                    </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     )
